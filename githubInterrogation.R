4b3b1a88c1d9d26dc3a0af8d14aadefa030b718f
Tiernan O'Connor <Tiernan@tiernans-mbp.home>
2018-11-03 16:25:10 +0000

    Code to get started

--- githubInterrogation.R
+++ githubInterrogation.R
@@ -1,1 +1,36 @@
-#first commit
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "gitInterrogation",
+                   key = "13be9ef2c3fd7874d6b7",
+                   secret = "eac1a3a55bab0b0d4a2f85356a795fc851be9137")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+#above code sourced from https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
+
