9bd9eaa65b65e3a27697fbc2db764de119013b64
Tiernan O'Connor <Tiernan@Tiernans-MacBook-Pro.local>
2018-11-05 15:29:41 +0000

    Data obtained from users added

--- githubInterrogation.R
+++ githubInterrogation.R
@@ -1,14 +1,11 @@
-#install.packages("jsonlite")
 library(jsonlite)
-#install.packages("httpuv")
 library(httpuv)
-#install.packages("httr")
 library(httr)
 
-# Can be github, linkedin etc depending on application
+#Interrogates github
 oauth_endpoints("github")
 
-# Change based on what you 
+#Change based on what you 
 myapp <- oauth_app(appname = "gitInterrogation",
                    key = "13be9ef2c3fd7874d6b7",
                    secret = "eac1a3a55bab0b0d4a2f85356a795fc851be9137")
@@ -34,3 +31,16 @@ gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
 
 #above code sourced from https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
 
+#Interrogate the Github API to extract data from my own github account
+myData = fromJSON("https://api.github.com/users/toconno5")
+myData$followers #dispalys number of followers
+myData$following #displays number of people I am following
+myData$public_repos #displays the number of repositories I have
+myData$bio #Displays my bio
+
+#Interrogate the Github API to extract data from another account by switching the username
+kennyc11Data = fromJSON("https://api.github.com/users/kennyc11")
+kennyc11Data$followers
+kennyc11Data$following 
+kennyc11Data$public_repos 
+kennyc11Data$bio 
